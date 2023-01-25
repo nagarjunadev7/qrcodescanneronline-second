3881b30dc61225f7911f60a7378f361ef4c14e23
nagarjuna <nagarjunarokkala@gmail.com>
2023-01-26 00:14:17 +0400

    first commit

---
+++ components/TopNavbar.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import styles from "../src/styles/TopNavbar.module.css";
+import mypic from "../public/logo.png";
+import Image from 'next/image'
+
+export default function TopNavbar() {
+  return (
+    <div className={styles.topnavbar}>
+      <ul>
+      <li>
+      <Link href="/">
+          <Image
+            src={mypic}
+            alt="qr code scanner online & generator" 
+            className={styles.mypic}
+          />
+        </Link>
+        </li> 
+        <li>
+          <Link href="/">
+            <h1 className={styles.mainheading}>Qr Code Scanner & Generator Online</h1>
+          </Link>
+          <p>Scan smarter, with our online QR code scanner</p>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import HamburgerMenu from "../../components/HamburgerMenu";
+import Navbar from "../../components/Navbar";
+import TopNavbar from "../../components/TopNavbar";
+import Scan from "../pages/Scan";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>QR Code Scanner Online </title>
+        <meta name="description" content="Scan QR codes quickly and easily with our QR code scanner online. Our user-friendly scanner is compatible with all devices and can be used for various purposes. Try it now for free." />
+        <meta name="keywords" content="QRcode,Qr code scanner online,bar code scanner online,barcode scanner,qrcode scanner,qrcode scanner mobile"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+     
+      <main>
+      <TopNavbar />
+      <Navbar />
+      <HamburgerMenu />
+      <Scan />
+      </main>
+    </div>
+  );
+}
